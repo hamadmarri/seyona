75ae342bf79d166fb36145214d4ab7b0be0099ff
Hamad Almarri <hamad.s.almarri@gmail.com>
2016-01-12 03:09:33 +0300

    update CountryServer for debuging purpose

--- scripts/country/CountryServer.js
+++ scripts/country/CountryServer.js
@@ -4,6 +4,7 @@ var io = require('socket.io')(http);
 var _ = require('lodash-node');
 
 var users = [];
+var findCount = 0;
 
 app.get('/', function (req, res){
   res.sendfile('index.html');
@@ -14,8 +15,17 @@ io.on('connection', function (socket) {
         
     // if this socket is already connected,
     // send a failed login message name
-    if (_.findIndex(users, { socket: socket.id }) !== -1) {
-      socket.emit('login_error', 'You are already connected.');
+    var index = _.findIndex(users, { socket: socket.id });
+    if (index !== -1) {
+      // socket.emit('login_error', 'You are already connected.');
+
+      socket.broadcast.emit('offline', users[index].countryPerson);
+      users[index].countryPerson.countryCode = countryPerson.countryCode;
+      
+      socket.emit('login_successful', _.pluck(users, 'countryPerson'));
+      socket.broadcast.emit('online', users[index].countryPerson);
+
+      return; 
     }
 
     // if this name is already registered,
@@ -38,34 +48,58 @@ io.on('connection', function (socket) {
   });
 
 
-  socket.on('logout', function (countryPerson) {
-    
-    console.log(countryPerson);
+  socket.on('logout', function () {
     
     var index = _.findIndex(users, { socket: socket.id });
 
-    users.splice(index, 1);
+    if (index !== -1) {
+      socket.broadcast.emit('offline', users[index].countryPerson);
+      users.splice(index, 1);
+      // console.log(countryPerson.name + ' disconnected');
+    }
 
-    socket.broadcast.emit('offline', countryPerson);
-
-    console.log(countryPerson.name + ' disconnected');
   });
 
 
-  socket.on('searching', function (query) {
+  socket.on('searching', function () {
     var index = _.findIndex(users, { socket: socket.id });
 
     users[index].countryPerson.status = "searching";
 
-    console.log(users[index].countryPerson.name + " is searching");
+    // console.log(users[index].countryPerson.name + " is searching");
   });
+
+
+  socket.on('busy', function () {
+    var index = _.findIndex(users, { socket: socket.id });
+
+    users[index].countryPerson.status = "busy";
+
+    // console.log(users[index].countryPerson.name + " is busy");
+  });
+
+
+  socket.on('incrementCallsCount', function () {
+    var index = _.findIndex(users, { socket: socket.id });
+
+    users[index].countryPerson.callsCount++;
+
+    // console.log(users[index].countryPerson.name + " have " + users[index].countryPerson.callsCount);
+  });
+
 
   socket.on('find', function (query) {
    
+   var index = _.findIndex(users, { socket: socket.id });
+   console.log('find ' + (++findCount) + " from " + index);
+
     var min = -1;
     var countryPerson;
 
     for (var i = 0; i < users.length; i++) {
+      
+      console.log(users[i].name + " " + users[i].countryPerson.status);
+
       if (users[i].countryPerson.countryCode == query.countryCode
             && users[i].countryPerson.status == 'searching'
             && users[i].socket != socket.id) {
@@ -86,10 +120,10 @@ io.on('connection', function (socket) {
 
     if (min != -1) {
       socket.emit('found', countryPerson);
-      console.log('found ' + countryPerson.name);
+      // console.log('found ' + countryPerson.name);
     } else {
       socket.emit('not_found', { });
-      console.log('not_found');
+      // console.log('not_found');
     }
 
   });
@@ -111,8 +145,10 @@ io.on('connection', function (socket) {
 
     var index = _.findIndex(users, { socket: socket.id });
     if (index !== -1) {
+
       socket.broadcast.emit('offline', users[index].name);
-      console.log(users[index].name + ' disconnected');
+      console.log(users[index].name + ' disconnected had ' 
+          + users[index].countryPerson.callsCount + ' calls');
 
       users.splice(index, 1);
     }
