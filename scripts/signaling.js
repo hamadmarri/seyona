56deaed844cd224c8ed6249357a7d83a31d32a5f
hamadmarri <hamad.s.almarri@gmail.com>
2016-02-12 18:05:51 +0300

    pushing signaling.js

--- scripts/signaling.js
+++ scripts/signaling.js
@@ -1,6 +1,6 @@
 angular.module('phonertcdemo')
   .factory('signaling', function (socketFactory) {
-    var socket = io.connect('http://seyona.com:3000/'); // http://192.168.100.5:3000/
+    var socket = io.connect('http://192.168.100.6:3000/'); // http://seyona.com:3000/
     
     var socketFactory = socketFactory({
       ioSocket: socket
@@ -10,7 +10,7 @@ angular.module('phonertcdemo')
   })
 
   .factory('signalingCountry', function (socketFactory) {
-    var socket = io.connect('http://seyona.com:3001/');
+    var socket = io.connect('http://192.168.100.6:3001/');
     
     var socketFactory = socketFactory({
       ioSocket: socket
@@ -20,7 +20,7 @@ angular.module('phonertcdemo')
   })
 
   .factory('signalingInterests', function (socketFactory) {
-    var socket = io.connect('http://seyona.com:3002/');
+    var socket = io.connect('http://192.168.100.6:3002/');
     
     var socketFactory = socketFactory({
       ioSocket: socket
